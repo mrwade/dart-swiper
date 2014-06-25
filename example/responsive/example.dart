de1e83e18b2c34cbcdf87f7a785974b14cabe2c8
Drone Build <support@drone.io>
2014-06-25 12:16:54 +0000

    auto commit from drone

--- example/responsive/example.dart
+++
@@ -1,7 +0,0 @@
-import 'dart:html';
-import 'package:swiper/swiper.dart';
-
-main() {
-  // Initialize the Swiper.
-  Swiper swiper = new Swiper(querySelector('.swiper'), autoHeightRatio: 0.666);
-}
